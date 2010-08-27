dd828d7c73c0b923631cba051354a8c3cd351c8f
Tomaz Kragelj <tkragelj@gmail.com>
2010-08-27 21:08:54 +0200

    Implemented categories and protocols processing.

--- Testing/GBProcessor-CommentsTesting.m
+++ Testing/GBProcessor-CommentsTesting.m
@@ -49,4 +49,68 @@
 	[comment2 verify];
 }
 
+#pragma mark Categories processing
+
+- (void)testProcessObjectsFromStore_shouldProcessCategoryComments {
+	// setup
+	GBProcessor *processor = [GBProcessor processorWithSettingsProvider:[GBTestObjectsRegistry mockSettingsProvider]];
+	OCMockObject *comment = [OCMockObject niceMockForClass:[GBComment class]];
+	GBStore *store = [GBTestObjectsRegistry storeWithCategoryWithComment:comment];
+	[[comment expect] processCommentWithStore:store];
+	// execute
+	[processor processObjectsFromStore:store];
+	// verify - we only check that comment receive process message here, details are tested with comments!
+	[comment verify];
+}
+
+- (void)testProcessObjectsFromStore_shouldProcessCategoryMethodComments {
+	// setup
+	GBProcessor *processor = [GBProcessor processorWithSettingsProvider:[GBTestObjectsRegistry mockSettingsProvider]];
+	OCMockObject *comment1 = [OCMockObject niceMockForClass:[GBComment class]];
+	OCMockObject *comment2 = [OCMockObject niceMockForClass:[GBComment class]];
+	GBCategoryData *category = [GBCategoryData categoryDataWithName:@"Category" className:@"Class"];
+	[category.methods registerMethod:[GBTestObjectsRegistry instanceMethodWithName:@"method1" comment:comment1]];
+	[category.methods registerMethod:[GBTestObjectsRegistry instanceMethodWithName:@"method2" comment:comment2]];
+	GBStore *store = [GBTestObjectsRegistry storeByPerformingSelector:@selector(registerCategory:) withObject:category];
+	[[comment1 expect] processCommentWithStore:store];
+	[[comment2 expect] processCommentWithStore:store];
+	// execute
+	[processor processObjectsFromStore:store];
+	// verify - we only check that comments receive process message here, details are tested with comments!
+	[comment1 verify];
+	[comment2 verify];
+}
+
+#pragma mark Protocols processing
+
+- (void)testProcessObjectsFromStore_shouldProcessProtocolComments {
+	// setup
+	GBProcessor *processor = [GBProcessor processorWithSettingsProvider:[GBTestObjectsRegistry mockSettingsProvider]];
+	OCMockObject *comment = [OCMockObject niceMockForClass:[GBComment class]];
+	GBStore *store = [GBTestObjectsRegistry storeWithProtocolWithComment:comment];
+	[[comment expect] processCommentWithStore:store];
+	// execute
+	[processor processObjectsFromStore:store];
+	// verify - we only check that comment receive process message here, details are tested with comments!
+	[comment verify];
+}
+
+- (void)testProcessObjectsFromStore_shouldProcessProtocolMethodComments {
+	// setup
+	GBProcessor *processor = [GBProcessor processorWithSettingsProvider:[GBTestObjectsRegistry mockSettingsProvider]];
+	OCMockObject *comment1 = [OCMockObject niceMockForClass:[GBComment class]];
+	OCMockObject *comment2 = [OCMockObject niceMockForClass:[GBComment class]];
+	GBProtocolData *protocol = [GBProtocolData protocolDataWithName:@"Protocol"];
+	[protocol.methods registerMethod:[GBTestObjectsRegistry instanceMethodWithName:@"method1" comment:comment1]];
+	[protocol.methods registerMethod:[GBTestObjectsRegistry instanceMethodWithName:@"method2" comment:comment2]];
+	GBStore *store = [GBTestObjectsRegistry storeByPerformingSelector:@selector(registerProtocol:) withObject:protocol];
+	[[comment1 expect] processCommentWithStore:store];
+	[[comment2 expect] processCommentWithStore:store];
+	// execute
+	[processor processObjectsFromStore:store];
+	// verify - we only check that comments receive process message here, details are tested with comments!
+	[comment1 verify];
+	[comment2 verify];
+}
+
 @end
