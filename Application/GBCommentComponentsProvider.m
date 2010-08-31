407944d2381f509c5799c70aa919ce2e870c4f1b
Tomaz Kragelj <tkragelj@gmail.com>
2010-08-31 21:32:53 +0200

    Refactored GBCommentComponentsProvider by making it return regex expressions instead of matching values itself.
    This makes the component much more versatile and allows the rest of the application to do whatever it desired with the regex - either match, get components etc.

--- Application/GBCommentComponentsProvider.m
+++ Application/GBCommentComponentsProvider.m
@@ -9,11 +9,17 @@
 #import "RegexKitLite.h"
 #import "GBCommentComponentsProvider.h"
 
+#define GBRETURN_ON_DEMAND(regex) \
+	static NSString *result = nil; \
+	if (!result) result = regex; \
+	return result;
+
+#pragma mark -
+
 @interface GBCommentComponentsProvider ()
 
-- (BOOL)string:(NSString *)string startsWithKeyword:(NSString *)keyword;
-- (BOOL)string:(NSString *)string matchesRegex:(NSString *)regex;
-@property (retain) NSCharacterSet *delimitersSet;
+- (NSString *)singleCaptureRegexForKeyword:(NSString *)keyword;
+- (NSString *)doubleCaptureRegexForKeyword:(NSString *)keyword;
 
 @end
 
@@ -27,67 +33,48 @@
 	return [[[self alloc] init] autorelease];
 }
 
-- (id)init {
-	self = [super init];
-	if (self) {
-		self.delimitersSet = [NSCharacterSet whitespaceCharacterSet];
-	}
-	return self;
-}
-
 #pragma mark Public interface
 
-- (BOOL)stringDefinesOrderedList:(NSString *)string {
-	NSString *regex = @"^\\s*[0-9]+\\.\\s+";	// optional WS, followed by list delimiter and dot, followed by at least one WS
-	return [self string:string matchesRegex:regex];
+- (NSString *)orderedListRegex {
+	GBRETURN_ON_DEMAND(@"^\\s*[-+*o]\\s+(.*)");
 }
 
-- (BOOL)stringDefinesUnorderedList:(NSString *)string {
-	NSString *regex = @"^\\s*[-+*o]\\s+";	// optional WS, followed by list delimiter, followed by at least one WS
-	return [self string:string matchesRegex:regex];
+- (NSString *)unorderedListRegex {
+	GBRETURN_ON_DEMAND(@"^\\s*[0-9]+\\.\\s+(.*)");
 }
 
-- (BOOL)stringDefinesWarning:(NSString *)string {
-	return [self string:string startsWithKeyword:@"warning"];
+- (NSString *)warningSectionRegex {
+	GBRETURN_ON_DEMAND([self singleCaptureRegexForKeyword:@"warning"]);
 }
 
-- (BOOL)stringDefinesBug:(NSString *)string {
-	return [self string:string startsWithKeyword:@"bug"];
+- (NSString *)bugSectionRegex {
+	GBRETURN_ON_DEMAND([self singleCaptureRegexForKeyword:@"bug"]);
 }
 
-- (BOOL)stringDefinesParameter:(NSString *)string {
-	return [self string:string startsWithKeyword:@"param"];
+- (NSString *)parameterDescriptionRegex {
+	GBRETURN_ON_DEMAND([self doubleCaptureRegexForKeyword:@"param"]);
 }
 
-- (BOOL)stringDefinesReturn:(NSString *)string {
-	return [self string:string startsWithKeyword:@"return"];
+- (NSString *)returnDescriptionRegex {
+	GBRETURN_ON_DEMAND([self singleCaptureRegexForKeyword:@"return"]);
 }
 
-- (BOOL)stringDefinesException:(NSString *)string {
-	return [self string:string startsWithKeyword:@"exception"];
+- (NSString *)exceptionDescriptionRegex {
+	GBRETURN_ON_DEMAND([self doubleCaptureRegexForKeyword:@"exception"]);
 }
 
-- (BOOL)stringDefinesCrossReference:(NSString *)string {
-	NSString *regex = @"^\\s*.(sa|see)";
-	return [self string:string matchesRegex:regex];
+- (NSString *)crossReferenceRegex {
+	GBRETURN_ON_DEMAND(@"^\\s*.(sa|see)\\s+(.*)");
 }
 
 #pragma mark Helper methods
 
-- (BOOL)string:(NSString *)string startsWithKeyword:(NSString *)keyword {
-	// Note that this method doesn't require special keyword prefix - it would take any character as long as it's followed by the given keyword and at least one delimiter char! This method is only valid for testing keywords that are followed by some text!
-	if ([keyword length] == 0) return NO;
-	if ([string length] < [keyword length] + 2) return NO;
-	NSString *regex = [NSString stringWithFormat:@"^\\s*.%@", keyword];
-	return [self string:string matchesRegex:regex];
+- (NSString *)singleCaptureRegexForKeyword:(NSString *)keyword {
+	return [NSString stringWithFormat:@"^\\s*.%@\\s+(.*)", keyword];
 }
 
-- (BOOL)string:(NSString *)string matchesRegex:(NSString *)regex {
-	return [string isMatchedByRegex:regex];
+- (NSString *)doubleCaptureRegexForKeyword:(NSString *)keyword {
+	return [NSString stringWithFormat:@"^\\s*.%@\\s+([^\\s]+)\\s+(.*)", keyword];
 }
-
-#pragma mark Properties
-
-@synthesize delimitersSet;
 
 @end
