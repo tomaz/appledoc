005e9ee750a82f3aeebe7d2222d5c657ba0748b9
Tomaz Kragelj <tkragelj@gmail.com>
2010-09-10 20:41:20 +0200

    Added more URL unit tests.

--- Testing/GBCommentsProcessor-LinkItemsTesting.m
+++ Testing/GBCommentsProcessor-LinkItemsTesting.m
@@ -16,7 +16,7 @@
 
 @implementation GBCommentsProcessorLinkItemsTesting
 
-#pragma mark General processing testing
+#pragma mark General processing & URL testing
 
 - (void)testProcesCommentWithStore_shouldGenerateParagraphIfNoneSpecifiedBefore {
 	// setup
@@ -60,6 +60,40 @@
 	// verify
 	assertThatInteger([comment.paragraphs count], equalToInteger(1));
 	[self assertParagraph:[comment.paragraphs objectAtIndex:0] containsItems:[GBParagraphTextItem class], @"prefix", [GBParagraphLinkItem class], @"http://gentlebytes.com", [GBParagraphTextItem class], @"suffix", nil];
+}
+
+#pragma mark URL testing
+
+- (void)testProcessCommentWithStore_url_shouldDetectVariousPrefixes {
+	// setup
+	GBCommentsProcessor *processor = [GBCommentsProcessor processorWithSettingsProvider:[GBTestObjectsRegistry mockSettingsProvider]];
+	GBComment *comment1 = [GBComment commentWithStringValue:@"http://gentlebytes.com"];
+	GBComment *comment2 = [GBComment commentWithStringValue:@"https://gentlebytes.com"];
+	GBComment *comment3 = [GBComment commentWithStringValue:@"ftp://user:pass@gentlebytes.com"];
+	GBComment *comment4 = [GBComment commentWithStringValue:@"file://gentlebytes.com"];
+	// execute
+	[processor processComment:comment1 withStore:[GBTestObjectsRegistry store]];
+	[processor processComment:comment2 withStore:[GBTestObjectsRegistry store]];
+	[processor processComment:comment3 withStore:[GBTestObjectsRegistry store]];
+	[processor processComment:comment4 withStore:[GBTestObjectsRegistry store]];
+	// verify
+	[self assertParagraph:[comment1.paragraphs objectAtIndex:0] containsItems:[GBParagraphLinkItem class], @"http://gentlebytes.com", nil];
+	[self assertParagraph:[comment2.paragraphs objectAtIndex:0] containsItems:[GBParagraphLinkItem class], @"https://gentlebytes.com", nil];
+	[self assertParagraph:[comment3.paragraphs objectAtIndex:0] containsItems:[GBParagraphLinkItem class], @"ftp://user:pass@gentlebytes.com", nil];
+	[self assertParagraph:[comment4.paragraphs objectAtIndex:0] containsItems:[GBParagraphLinkItem class], @"file://gentlebytes.com", nil];
+}
+
+- (void)testProcessCommentWithStore_url_shouldIgnoreUnknownPrefixes {
+	// setup
+	GBCommentsProcessor *processor = [GBCommentsProcessor processorWithSettingsProvider:[GBTestObjectsRegistry mockSettingsProvider]];
+	GBComment *comment1 = [GBComment commentWithStringValue:@"appledoc://gentlebytes.com"];
+	GBComment *comment2 = [GBComment commentWithStringValue:@"htp://gentlebytes.com"];
+	// execute
+	[processor processComment:comment1 withStore:[GBTestObjectsRegistry store]];
+	[processor processComment:comment2 withStore:[GBTestObjectsRegistry store]];
+	// verify
+	[self assertParagraph:[comment1.paragraphs objectAtIndex:0] containsItems:[GBParagraphTextItem class], @"appledoc://gentlebytes.com", nil];
+	[self assertParagraph:[comment2.paragraphs objectAtIndex:0] containsItems:[GBParagraphTextItem class], @"htp://gentlebytes.com", nil];
 }
 
 @end
