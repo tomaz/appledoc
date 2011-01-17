f8a381ee5369918c18f335ad6bc2d1b6f48b1a67
Tomaz Kragelj <tkragelj@gmail.com>
2011-01-17 14:30:49 +0100

    Incremented build number to 514.

--- Application/GBApplicationStringsProvider.m
+++ Application/GBApplicationStringsProvider.m
@@ -125,7 +125,7 @@
 		result = [[NSMutableDictionary alloc] init];
 		[result setObject:@"appledoc" forKey:@"tool"];
 		[result setObject:@"2.0b3" forKey:@"version"];
-		[result setObject:@"512" forKey:@"build"];
+		[result setObject:@"514" forKey:@"build"];
 		[result setObject:@"http://appledoc.gentlebytes.com" forKey:@"homepage"];
 	}
 	return result;
