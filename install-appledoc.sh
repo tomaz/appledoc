be3aa2c0e0f249cd4d7a5b304cf73ca8da42ce19
Tomaz Kragelj <tkragelj@gmail.com>
2011-12-15 13:35:44 +0100

    Fixed install script to better handle paths with whitespace, thanks to koyachi. Closes #159.

--- install-appledoc.sh
+++ install-appledoc.sh
@@ -35,7 +35,7 @@ echo "Building..."
 xcodebuild -target appledoc -configuration Release install
 
 echo "Installing binary to $BINARY_DIR"
-cp /tmp/appledoc.dst/usr/local/bin/appledoc $BINARY_DIR
+cp /tmp/appledoc.dst/usr/local/bin/appledoc "$BINARY_DIR"
 
 echo "Copying templates to $TEMPLATES_DIR"
-cp -R Templates/ $TEMPLATES_DIR
+cp -R Templates/ "$TEMPLATES_DIR"
