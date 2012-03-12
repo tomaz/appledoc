f33593ad0cddbac8075910b45413a4b53a54ba36
Tomaz Kragelj <tkragelj@gmail.com>
2012-03-12 11:24:00 +0100

    Cleanup for fresh start.

---
+++ appledoc/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  appledoc
+//
+//  Created by Tomaž Kragelj on 3/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
