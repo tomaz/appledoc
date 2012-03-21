581a28207d11885297138c62a6459172b0117d4a
Tomaz Kragelj <tkragelj@gmail.com>
2012-03-21 13:01:41 +0100

    Added struct parsing.

--- appledoc/Parsing/ObjectiveC/ObjectiveCStructState.m
+++ appledoc/Parsing/ObjectiveC/ObjectiveCStructState.m
@@ -11,6 +11,27 @@
 @implementation ObjectiveCStructState
 
 - (NSUInteger)parseStream:(TokensStream *)stream forParser:(ObjectiveCParser *)parser store:(Store *)store {
+	// Name is required, but skip everything else until '{'. Then match all definitions of type `type(s) <def>=value,` or `type(s) <def>;`.
+	if ([stream matches:@"struct", nil]) {
+		NSString *name = stream.current.stringValue;
+		LogParDebug(@"Matched struct %@.", name);
+		NSMutableString *declaration = [NSMutableString stringWithFormat:@"struct %@ {\n", name];
+		[self skipStream:stream until:@"{" block:^(PKToken *token) { }];
+		[self skipStream:stream until:@"}" block:^(PKToken *token) {
+			LogParDebug(@"Matched %@", token);
+			if ([token matches:@";"] || [token matches:@"="]) {
+				PKToken *nameToken = [stream la:token offset:-1];
+				[declaration appendFormat:@"%@\n", nameToken.stringValue];
+			}
+		}];
+		[declaration appendString:@"}"];
+		LogParVerbose(@"%@", declaration);
+		[parser popState];
+	} else {
+		[stream consume:1];
+		[parser popState];
+		return GBResultFailedMatch;
+	}
 	return GBResultOk;
 }
 
