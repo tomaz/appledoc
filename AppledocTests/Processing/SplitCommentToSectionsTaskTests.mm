c444bfa2a887e27af7ad2f18c54ecfbf7bc34271
Tomaz Kragelj <tkragelj@gmail.com>
2012-11-24 10:11:33 +0100

    Added back unit tests for code blocks.

--- AppledocTests/Processing/SplitCommentToSectionsTaskTests.mm
+++ AppledocTests/Processing/SplitCommentToSectionsTaskTests.mm
@@ -73,4 +73,163 @@ describe(@"abstract:", ^{
 	});
 });
 
+describe(@"normal text:", ^{
+	it(@"should convert second paragraph to discussion", ^{
+		runWithTask(^(SplitCommentToSectionsTask *task, id comment) {
+			// setup
+			setupComment(comment, @"abstract\n\nsecond");
+			// execute
+			[task processComment:comment];
+			// verify
+			GBSections.count should equal(2);
+			GBSections[0] should equal(@"abstract");
+			GBSections[1] should equal(@"second");
+		});
+	});
+
+	it(@"should convert second and subsequent paragraphs to discussion", ^{
+		runWithTask(^(SplitCommentToSectionsTask *task, id comment) {
+			// setup
+			setupComment(comment, @"abstract\n\nsecond\n\nthird");
+			// execute
+			[task processComment:comment];
+			// verify
+			GBSections.count should equal(2);
+			GBSections[0] should equal(@"abstract");
+			GBSections[1] should equal(@"second\n\nthird");
+		});
+	});
+
+	it(@"should handle multiple paragraphs with mutliple lines", ^{
+		runWithTask(^(SplitCommentToSectionsTask *task, id comment) {
+			// setup
+			setupComment(comment, @"abstract\n\nline one\nline two\nline three\n\nthird paragraph\nand line two");
+			// execute
+			[task processComment:comment];
+			// verify
+			GBSections.count should equal(2);
+			GBSections[0] should equal(@"abstract");
+			GBSections[1] should equal(@"line one\nline two\nline three\n\nthird paragraph\nand line two");
+		});
+	});
+});
+
+describe(@"block code:", ^{
+#define GBReplace(t) [t gb_stringByReplacing:@{ @"[": info[@"start"], @"]": info[@"end"], @"--": info[@"marker"] }]
+	sharedExamplesFor(@"block code", ^(NSDictionary *info){
+		it(@"should append block code to previous paragraph", ^{
+			runWithTask(^(SplitCommentToSectionsTask *task, id comment) {
+				// setup
+				setupComment(comment, GBReplace(@"abstract\n\nnormal line\n\n[--block code]"));
+				// execute
+				[task processComment:comment];
+				// verify
+				GBSections.count should equal(3);
+				GBSections[0] should equal(@"abstract");
+				GBSections[1] should equal(@"normal line");
+				GBSections[2] should equal(GBReplace(@"[--block code]"));
+			});
+		});
+
+		it(@"should append all block code lines to previous paragraph", ^{
+			runWithTask(^(SplitCommentToSectionsTask *task, id comment) {
+				// setup
+				setupComment(comment, GBReplace(@"abstract\n\nnormal line\n\n[--line 1\n--line 2]"));
+				// execute
+				[task processComment:comment];
+				// verify
+				GBSections.count should equal(3);
+				GBSections[0] should equal(@"abstract");
+				GBSections[1] should equal(@"normal line");
+				GBSections[2] should equal(GBReplace(@"[--line 1\n--line 2]"));
+			});
+		});
+
+		it(@"should append multiple block code sections to previous paragraph", ^{
+			runWithTask(^(SplitCommentToSectionsTask *task, id comment) {
+				// setup
+				setupComment(comment, GBReplace(@"abstract\n\nnormal line\n\n[--line 1]\n\n[--line 2\n--line 3]"));
+				// execute
+				[task processComment:comment];
+				// verify
+				GBSections.count should equal(4);
+				GBSections[0] should equal(@"abstract");
+				GBSections[1] should equal(@"normal line");
+				GBSections[2] should equal(GBReplace(@"[--line 1]"));
+				GBSections[3] should equal(GBReplace(@"[--line 2\n--line 3]"));
+			});
+		});
+
+		it(@"should append multiple block code sections delimited with normal paragraphs", ^{
+			runWithTask(^(SplitCommentToSectionsTask *task, id comment) {
+				// setup
+				setupComment(comment, GBReplace(@"abstract\n\nnormal line 1\n\n[--line 1]\n\nnormal line 2\n\n[--line 2]"));
+				// execute
+				[task processComment:comment];
+				// verify
+				GBSections.count should equal(5);
+				GBSections[0] should equal(@"abstract");
+				GBSections[1] should equal(@"normal line 1");
+				GBSections[2] should equal(GBReplace(@"[--line 1]"));
+				GBSections[3] should equal(@"normal line 2");
+				GBSections[4] should equal(GBReplace(@"[--line 2]"));
+			});
+		});
+
+		it(@"should continue normal paragraph if not delimited with empty line", ^{
+			runWithTask(^(SplitCommentToSectionsTask *task, id comment) {
+				// setup
+				setupComment(comment, GBReplace(@"abstract\n\nnormal line\n[--continue line]"));
+				// execute
+				[task processComment:comment];
+				// verify
+				GBSections.count should equal(2);
+				GBSections[0] should equal(@"abstract");
+				GBSections[1] should equal(GBReplace(@"normal line\n[--continue line]"));
+			});
+		});
+
+		it(@"should keep all formatting after initial code block marker", ^{
+			runWithTask(^(SplitCommentToSectionsTask *task, id comment) {
+				// setup
+				setupComment(comment, GBReplace(@"abstract\n\nnormal line\n\n[--line 1\n--\tline 2\n--    line 3]"));
+				// execute
+				[task processComment:comment];
+				// verify
+				GBSections.count should equal(3);
+				GBSections[0] should equal(@"abstract");
+				GBSections[1] should equal(@"normal line");
+				GBSections[2] should equal(GBReplace(@"[--line 1\n--\tline 2\n--    line 3]"));
+			});
+		});
+	});
+
+	describe(@"delimited with tab:", ^{
+		beforeEach(^{
+			[[SpecHelper specHelper] sharedExampleContext][@"start"] = @"";
+			[[SpecHelper specHelper] sharedExampleContext][@"marker"] = @"\t";
+			[[SpecHelper specHelper] sharedExampleContext][@"end"] = @"";
+		});
+		itShouldBehaveLike(@"block code");
+	});
+
+	describe(@"delimited with spaces:", ^{
+		beforeEach(^{
+			[[SpecHelper specHelper] sharedExampleContext][@"start"] = @"";
+			[[SpecHelper specHelper] sharedExampleContext][@"marker"] = @"    ";
+			[[SpecHelper specHelper] sharedExampleContext][@"end"] = @"";
+		});
+		itShouldBehaveLike(@"block code");
+	});
+
+	describe(@"fenced code blocks:", ^{
+		beforeEach(^{
+			[[SpecHelper specHelper] sharedExampleContext][@"start"] = @"```";
+			[[SpecHelper specHelper] sharedExampleContext][@"marker"] = @"";
+			[[SpecHelper specHelper] sharedExampleContext][@"end"] = @"```";
+		});
+		itShouldBehaveLike(@"block code");
+	});
+});
+
 TEST_END
