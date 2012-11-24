e087215079e684e7081b8fe9cc29c757fe70da85
Tomaz Kragelj <tkragelj@gmail.com>
2012-11-24 09:59:35 +0100

    Back to building (but not yet working :)

---
+++ appledoc/Processing/RegisterCommentComponentsTask.h
@@ -0,0 +1,17 @@
+//
+//  RegisterCommentComponentsTask.h
+//  appledoc
+//
+//  Created by Tomaz Kragelj on 8/12/12.
+//  Copyright (c) 2012 Tomaz Kragelj. All rights reserved.
+//
+
+#import "ProcessorTask.h"
+
+/** Implements concrete ProcessorTask for splitting comment string into components.
+ 
+ The purpose of this task is to split single text into individual components such as abstract, discussion, and various directives - parameters, exceptions, related objects etc. All components are stored as part of the given CommentInfo properties so they are available for subsequent processing tasks.
+ */
+@interface RegisterCommentComponentsTask : ProcessorTask
+
+@end
--- appledoc/Processing/SplitCommentToSectionsTask.m
+++ appledoc/Processing/SplitCommentToSectionsTask.m
@@ -12,12 +12,9 @@
 #import "CommentNamedSectionInfo.h"
 #import "SplitCommentToSectionsTask.h"
 
-@interface ProcessComponentsData : NSObject
-@property (nonatomic, strong) NSMutableString *builder;
-@property (nonatomic, strong) NSMutableArray *sections;
+@interface SplitCommentToSectionsTask ()
+@property (nonatomic, strong) CommentInfo *comment;
 @end
-
-@implementation ProcessComponentsData @end
 
 #pragma mark -
 
@@ -27,82 +24,72 @@
 
 - (NSInteger)processComment:(CommentInfo *)comment {
 	LogVerbose(@"Processing comment '%@' for components...", [comment.sourceString gb_description]);
-	
-	// Prepare internal data.
-	ProcessComponentsData *data = [[ProcessComponentsData alloc] init];
-	data.builder = [@"" mutableCopy];
-	data.sections = [@[] mutableCopy];
-	
-	// Parse comment string into sections.
-	[self parseSectionsFromComment:comment toData:data];
-	[self registerSectionFromBuilderInData:data startNewSection:NO];
-	if (data.sections.count == 0) return GBResultOk;
-	
-	// Process sections into comment components.
-	[self registerAbstractFromData:data toComment:comment];
-	[self registerDiscussionFromData:data toComment:comment];
-	[self registerMethodFromData:data toComment:comment];
+	NSMutableString *builder = [@"" mutableCopy];
+	self.comment = comment;
+	self.comment.sourceSections = [@[] mutableCopy];
+	[self parseSectionsWithBuilder:builder];
+	[self registerSectionFromBuilder:builder startNewSection:NO];
 	return GBResultOk;
 }
 
 #pragma mark - Splitting source string to sections
 
-- (void)parseSectionsFromComment:(CommentInfo *)comment toData:(ProcessComponentsData *)data {
+- (void)parseSectionsWithBuilder:(NSMutableString *)builder {
 	LogDebug(@"Parsing comment string into sections...");
 	NSRegularExpression *expression = [NSRegularExpression gb_emptyLineMatchingExpression];
-	NSString *sourceString = comment.sourceString;
+	NSString *sourceString = self.comment.sourceString;
 	__weak SplitCommentToSectionsTask *bself = self;
 	__block NSUInteger index = 0;
 	[expression gb_allMatchesIn:sourceString match:^(NSTextCheckingResult *match, NSUInteger idx, BOOL *stop) {
 		NSRange range = NSMakeRange(index, match.range.location - index);
 		NSString *section = [sourceString substringWithRange:range];
-		[bself parseSectionsFromString:section toData:data];
+		[bself parseSectionsFromString:section toBuilder:builder];
 		index = match.range.location + match.range.length;
 	}];
 	NSString *remainingString = [sourceString substringFromIndex:index];
-	[self parseSectionsFromString:remainingString toData:data];
+	[self parseSectionsFromString:remainingString toBuilder:builder];
 }
 
-- (void)parseSectionsFromString:(NSString *)sectionString toData:(ProcessComponentsData *)data {
+- (void)parseSectionsFromString:(NSString *)sectionString toBuilder:(NSMutableString *)builder {
 	if (sectionString.length == 0) return;
-	if ([self parseCodeBlockFromString:sectionString toData:data]) return;
-	if ([self parseStyledSectionFromString:sectionString toData:data]) return;
-	if ([self parseMethodSectionFromString:sectionString toData:data]) return;
+	if ([self parseCodeBlockFromString:sectionString toBuilder:builder]) return;
+	if ([self parseStyledSectionFromString:sectionString toBuilder:builder]) return;
+	if ([self parseMethodSectionFromString:sectionString toBuilder:builder]) return;
 	
 	// If this is the first paragraph to be registered, take it as abstract and create single section out of it.
-	if (data.sections.count == 0) {
+	if (self.comment.sourceSections.count == 0) {
 		LogDebug(@"Detected abstract '%@'...", [sectionString gb_description]);
-		[data.builder appendString:sectionString];
-		[self registerSectionFromBuilderInData:data startNewSection:YES];
+		[builder appendString:sectionString];
+		[self registerSectionFromBuilder:builder startNewSection:YES];
 		return;
 	}
 	
 	// Otherwise append "normal" paragraph to current section builder to be registered later on.
 	LogDebug(@"Appending paragraph '%@'...", [sectionString gb_description]);
-	if (data.builder.length > 0) [data.builder appendString:@"\n\n"];
-	[data.builder appendString:[sectionString gb_stringByTrimmingNewLines]];
+	if (builder.length > 0) [builder appendString:@"\n\n"];
+	[builder appendString:[sectionString gb_stringByTrimmingNewLines]];
 }
 
-- (BOOL)parseCodeBlockFromString:(NSString *)sectionString toData:(ProcessComponentsData *)data {
+- (BOOL)parseCodeBlockFromString:(NSString *)sectionString toBuilder:(NSMutableString *)builder {
 	// If current section represents code block, tread whole section as code block. Note that this doesn't work for empty strings!
 	if (![self isStringCodeBlock:sectionString]) return NO;
-	[self registerSectionFromBuilderInDataIfNeeded:data startNewSection:YES];
-	[data.builder appendString:sectionString];
-	[self registerSectionFromBuilderInDataIfNeeded:data startNewSection:YES];
+	[self registerSectionFromBuilderIfNeeded:builder startNewSection:YES];
+	[builder appendString:sectionString];
+	[self registerSectionFromBuilder:builder startNewSection:YES];
 	return YES;
 }
 
-- (BOOL)parseStyledSectionFromString:(NSString *)sectionString toData:(ProcessComponentsData *)data {
+- (BOOL)parseStyledSectionFromString:(NSString *)sectionString toBuilder:(NSMutableString *)builder {
 	// If current section represents @warning and @bug, treat whole section string as styled section.
 	NSTextCheckingResult *styledSectionMatch = [[NSRegularExpression gb_styledSectionDelimiterMatchingExpression] gb_firstMatchIn:sectionString];
 	if (![styledSectionMatch gb_isMatchedAtStart]) return NO;
-	[self registerSectionFromBuilderInDataIfNeeded:data startNewSection:YES];
-	[data.builder appendString:sectionString];
-	[self registerSectionFromBuilderInData:data startNewSection:NO];
+	[self registerSectionFromBuilderIfNeeded:builder startNewSection:YES];
+	[builder appendString:sectionString];
+	[self registerSectionFromBuilder:builder startNewSection:NO];
 	return YES;
 }
 
-- (BOOL)parseMethodSectionFromString:(NSString *)sectionString toData:(ProcessComponentsData *)data {
+- (BOOL)parseMethodSectionFromString:(NSString *)sectionString toBuilder:(NSMutableString *)builder {
 	// If current "paragraph" starts with method section directive, we must split the whole string into individual sections, each directive starting new section.
 	NSRegularExpression *expression = [NSRegularExpression gb_methodSectionDelimiterMatchingExpression];
 	NSArray *sectionMatches = [expression gb_allMatchesIn:sectionString];
@@ -110,130 +97,36 @@
 	
 	__weak SplitCommentToSectionsTask *bself = self;
 	__block NSUInteger lastMatchLocation = 0;
-	[self registerSectionFromBuilderInDataIfNeeded:data startNewSection:YES];
+	[self registerSectionFromBuilderIfNeeded:builder startNewSection:YES];
 	[sectionMatches enumerateObjectsUsingBlock:^(NSTextCheckingResult *match, NSUInteger idx, BOOL *stop) {
 		if (idx == 0) return;
 		NSString *currentSectionString = [[match gb_prefixFromIndex:lastMatchLocation in:sectionString] gb_stringByTrimmingNewLines];
-		[data.builder appendString:currentSectionString];
-		[bself registerSectionFromBuilderInData:data startNewSection:YES];
+		[builder appendString:currentSectionString];
+		[bself registerSectionFromBuilder:builder startNewSection:YES];
 		lastMatchLocation = match.range.location;
 	}];
 	
 	// Register remaining section, but keep the string so we can append subsequent paragraphs to it.
 	NSString *remainingSectionString = [[sectionString substringFromIndex:lastMatchLocation] gb_stringByTrimmingNewLines];
-	[data.builder appendString:remainingSectionString];
-	[self registerSectionFromBuilderInData:data startNewSection:NO];
+	[builder appendString:remainingSectionString];
+	[self registerSectionFromBuilder:builder startNewSection:NO];
 	return YES;
 }
 
-- (BOOL)registerSectionFromBuilderInDataIfNeeded:(ProcessComponentsData *)data startNewSection:(BOOL)startNew {
- 	if (data.builder.length == 0) return NO;
-	[self registerSectionFromBuilderInData:data startNewSection:startNew];
+- (BOOL)registerSectionFromBuilderIfNeeded:(NSMutableString *)builder startNewSection:(BOOL)startNew {
+	// Returns YES if section was registered, NO otherwise.
+ 	if (builder.length == 0) return NO;
+	[self registerSectionFromBuilder:builder startNewSection:startNew];
 	return YES;
 }
 
-- (void)registerSectionFromBuilderInData:(ProcessComponentsData *)data startNewSection:(BOOL)startNew {
+- (void)registerSectionFromBuilder:(NSMutableString *)builder startNewSection:(BOOL)startNew {
 	// Note that we need to handle special case where current builder string is the same pointer as last object; in such case we can ignore it, but we do need to clear the string!
-	if (data.builder.length == 0) return;
-	if (data.builder == data.sections.lastObject) { data.builder=[@"" mutableCopy]; return; }
-	LogDebug(@"Registering section '%@'...", [data.builder gb_description]);
-	[data.sections addObject:data.builder];
-	if (startNew) data.builder = [@"" mutableCopy];
-}
-
-#pragma mark - Registering sections to comment
-
-- (void)registerAbstractFromData:(ProcessComponentsData *)data toComment:(CommentInfo *)comment {
-	LogDebug(@"Registering abstract...");
-	CommentComponentInfo *component = [self componentInfoFromString:data.sections[0]];
-	[comment setCommentAbstract:component];
-	[data.sections removeObjectAtIndex:0];
-}
-
-- (void)registerDiscussionFromData:(ProcessComponentsData *)data toComment:(CommentInfo *)comment {
-	// Register all discussion sections - up until first parameter related section.
-	NSRegularExpression *expression = [NSRegularExpression gb_methodSectionDelimiterMatchingExpression];
-	CommentSectionInfo *discussion = nil;
-	while (data.sections.count > 0) {
-		NSString *sectionString = data.sections[0];
-		NSTextCheckingResult *match = [expression gb_firstMatchIn:sectionString];
-		if (match && match.range.location == 0) break;
-		if (!discussion) discussion = [[CommentSectionInfo alloc] init];
-		CommentComponentInfo *component = [self componentInfoFromString:sectionString];
-		[discussion.sectionComponents addObject:component];
-		[data.sections removeObjectAtIndex:0];
-	}
-	if (discussion) [comment setCommentDiscussion:discussion];
-}
-
-- (void)registerMethodFromData:(ProcessComponentsData *)data toComment:(CommentInfo *)comment {
-	NSMutableArray *parameters = nil;
-	NSMutableArray *exceptions = nil;
-	CommentSectionInfo *result = nil;
-	CommentSectionInfo *lastSection = nil;
-	while (data.sections.count > 0) {
-		NSString *sectionString = data.sections[0];
-		if ([self matchParameterSectionFromString:sectionString sections:data.sections toArray:&parameters]) continue;
-		if ([self matchExceptionSectionFromString:sectionString sections:data.sections toArray:&exceptions]) continue;
-		if ([self matchReturnSectionFromString:sectionString sections:data.sections toInfo:&result]) continue;
-		break;
-	}
-	if (parameters) [comment setCommentParameters:parameters];
-	if (exceptions) [comment setCommentExceptions:exceptions];
-	if (result) [comment setCommentReturn:result];
-}
-
-#pragma mark - Matching method directives
-
-- (BOOL)matchParameterSectionFromString:(NSString *)string sections:(NSMutableArray *)sections toArray:(NSMutableArray **)array {
-	NSRegularExpression *expression = [NSRegularExpression gb_paramMatchingExpression];
-	return [self matchNamedMethodSectionFromString:string expression:expression sections:sections toArray:array];
-}
-
-- (BOOL)matchExceptionSectionFromString:(NSString *)string sections:(NSMutableArray *)sections toArray:(NSMutableArray **)array {
-	NSRegularExpression *expression = [NSRegularExpression gb_exceptionMatchingExpression];
-	return [self matchNamedMethodSectionFromString:string expression:expression sections:sections toArray:array];
-}
-
-- (BOOL)matchReturnSectionFromString:(NSString *)string sections:(NSMutableArray *)sections toInfo:(CommentSectionInfo **)dest {
-	NSRegularExpression *expression = [NSRegularExpression gb_returnMatchingExpression];
-	return [self matchSimpleSectionFromString:string expression:expression sections:sections toInfo:dest];
-}
-
-- (BOOL)matchNamedMethodSectionFromString:(NSString *)string expression:(NSRegularExpression *)expression sections:(NSMutableArray *)sections toArray:(NSMutableArray **)array {
-	NSTextCheckingResult *match = [expression gb_firstMatchIn:string];
-	if (!match) return NO;
-	NSString *description = [match gb_remainingStringIn:string];
-	CommentComponentInfo *component = [self componentInfoFromString:description];
-	CommentNamedSectionInfo *info = [[CommentNamedSectionInfo alloc] init];
-	[info setSectionName:[match gb_stringAtIndex:2 in:string]];
-	[info.sectionComponents addObject:component];
-	if (!*array) *array = [@[] mutableCopy];
-	[*array addObject:info];
-	[sections removeObjectAtIndex:0];
-	return YES;
-}
-
-- (BOOL)matchSimpleSectionFromString:(NSString *)string expression:(NSRegularExpression *)expression sections:(NSMutableArray *)sections toInfo:(CommentSectionInfo **)dest {
-	NSTextCheckingResult *match = [expression gb_firstMatchIn:string];
-	if (!match) return NO;
-	NSString *description = [match gb_remainingStringIn:string];
-	CommentComponentInfo *component = [self componentInfoFromString:description];
-	CommentSectionInfo *info = [[CommentSectionInfo alloc] init];
-	[info.sectionComponents addObject:component];
-	*dest = info;
-	[sections removeObjectAtIndex:0];
-	return YES;
-}
-
-#pragma mark - Comment components handling
-
-- (CommentComponentInfo *)componentInfoFromString:(NSString *)string {
-	LogDebug(@"Creating component for %@...", string);
-	if ([string hasPrefix:@"@warning"]) return [CommentWarningComponentInfo componentWithSourceString:string];
-	if ([string hasPrefix:@"@bug"]) return [CommentBugComponentInfo componentWithSourceString:string];
-	if ([self isStringCodeBlock:string]) return [CommentCodeBlockComponentInfo componentWithSourceString:string];
-	return [CommentComponentInfo componentWithSourceString:string];
+	if (builder.length == 0) return;
+	if (builder == self.comment.sourceSections.lastObject) { [builder setString:@""]; return; }
+	LogDebug(@"Registering section '%@'...", [builder gb_description]);
+	[self.comment.sourceSections addObject:builder];
+	if (startNew) [builder setString:@""];
 }
 
 @end
