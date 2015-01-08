6275698488c0a76158b5bc3143cff18a2cf1ec57
Adam D <kirbylover4000@gmail.com>
2015-01-09 02:31:56 +1030

    Don't throw a JS error if there's no jump-to on the page.

--- Templates/html/js/script.js
+++ Templates/html/js/script.js
@@ -6,6 +6,10 @@ if (navigator.userAgent.toLowerCase().indexOf("xcode") != -1) {
 	document.documentElement.classList.add("xcode");
 }
 
-$("#jump-to").addEventListener("change", function(e) {
-	location.hash = this.options[this.selectedIndex].value;
-});
+var jumpTo = $("#jump-to");
+
+if (jumpTo) {
+	jumpTo.addEventListener("change", function(e) {
+		location.hash = this.options[this.selectedIndex].value;
+	});
+}
