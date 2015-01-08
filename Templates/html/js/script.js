8d8e172de4ecf38e5ce20b42d061af931abcfea5
Adam D <kirbylover4000@gmail.com>
2015-01-09 02:07:47 +1030

    Remove code to toggle the TOC; there is no TOC implemented in the prior commit.

--- Templates/html/js/script.js
+++ Templates/html/js/script.js
@@ -6,15 +6,6 @@ if (navigator.userAgent.toLowerCase().indexOf("xcode") != -1) {
 	document.documentElement.classList.add("xcode");
 }
 
-if (localStorage["hideTOC"]) {
-	document.documentElement.classList.add("hide-toc");
-}
-
-$("#toc-button").addEventListener("click", function(e) {
-	e.preventDefault();
-	document.documentElement.classList.toggle("hide-toc");
-});
-
 $("#jump-to").addEventListener("change", function(e) {
 	location.hash = this.options[this.selectedIndex].value;
 });
