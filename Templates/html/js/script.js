e198e1e6fffcc4cd569a0da3caad824adb34e4ee
Adam D <kirbylover4000@gmail.com>
2015-01-09 19:01:07 +1030

    Implement toggling of sections, as seen in Xcode 6's docs.

--- Templates/html/js/script.js
+++ Templates/html/js/script.js
@@ -13,3 +13,46 @@ if (jumpTo) {
 		location.hash = this.options[this.selectedIndex].value;
 	});
 }
+
+function hashChanged() {
+	if (/^#\/\/api\//.test(location.hash)) {
+		var element = document.querySelector("a[name='" + location.hash.substring(1) + "']");
+
+		if (!element) {
+			return;
+		}
+
+		element = element.parentNode;
+
+		element.classList.remove("hide");
+		fixScrollPosition(element);
+	}
+}
+
+function fixScrollPosition(element) {
+	var scrollTop = element.offsetTop - 150;
+	document.documentElement.scrollTop = scrollTop;
+	document.body.scrollTop = scrollTop;
+}
+
+[].forEach.call(document.querySelectorAll(".section-method"), function(element) {
+	element.classList.add("hide");
+
+	element.querySelector(".method-title a").addEventListener("click", function(e) {
+		var info = element.querySelector(".method-info"),
+			infoContainer = element.querySelector(".method-info-container");
+
+		element.classList.add("animating");
+		info.style.height = (infoContainer.clientHeight + 40) + "px";
+		fixScrollPosition(element);
+		element.classList.toggle("hide");
+
+		setTimeout(function() {
+			element.classList.remove("animating");
+			info.style.height = "auto";
+		}, 300);
+	});
+});
+
+window.addEventListener("hashchange", hashChanged);
+hashChanged();
