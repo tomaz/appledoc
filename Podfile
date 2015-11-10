fa906fac2adc384a60f077219e0fc3bac6dc5de4
Tomaz Kragelj <tkragelj@gmail.com>
2015-11-10 15:01:19 +0100

    Removed cocoapods from unit tests.

--- Podfile
+++
@@ -1,8 +0,0 @@
-platform :osx, '10.10'
-
-use_frameworks!
-
-target 'tests' do
-	pod 'Quick', '~> 0.8.0'
-	pod 'Nimble', '~> 3.0.0'
-end
--- Pods/Target Support Files/Pods-tests/Pods-tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-
-FOUNDATION_EXPORT double Pods_testsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_testsVersionString[];
-
