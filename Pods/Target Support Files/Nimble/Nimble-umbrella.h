93c59fa59cf3aee216597183e67c6842079a422a
Tomaz Kragelj <tkragelj@gmail.com>
2015-11-10 15:01:19 +0100

    Added travis support.

--- Pods/Target Support Files/Nimble/Nimble-umbrella.h
+++
@@ -1,9 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-#import "Nimble.h"
-#import "DSL.h"
-#import "NMBExceptionCapture.h"
-
-FOUNDATION_EXPORT double NimbleVersionNumber;
-FOUNDATION_EXPORT const unsigned char NimbleVersionString[];
-
